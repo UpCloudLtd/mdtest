d26deaa2f051b735e7175ba2b3df49f0ea66c609
Toni Kangas <toni.kangas@upcloud.com>
2022-09-20 21:38:03 +0300

    test: add testdata for failing test runs

---
+++ cmd/cmd_internal_test.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"testing"
+)
+
+func TestRoot_testdata(t *testing.T) {
+	for _, test := range []struct {
+		testPath string
+		exitCode int
+	}{
+		{
+			testPath: "../testdata/fail_expected_0_got_3.md",
+			exitCode: 1,
+		},
+		{
+			testPath: "../testdata/fail_expected_1_got_0.md",
+			exitCode: 1,
+		},
+		{
+			testPath: "../testdata/success_expected_0_got_0.md",
+			exitCode: 0,
+		},
+		{
+			testPath: "../testdata",
+			exitCode: 2,
+		},
+	} {
+		test := test
+		t.Run(test.testPath, func(t *testing.T) {
+			rootCmd.SetArgs([]string{test.testPath})
+			exitCode := Execute()
+			if exitCode != test.exitCode {
+				t.Errorf("Expected exit code %d, got %d", test.exitCode, exitCode)
+			}
+		})
+	}
+}
---
+++ testdata/fail_expected_1_got_0.md
@@ -0,0 +1,5 @@
+# Fail: expexted 1, got 0
+
+```sh exit_code=1
+exit 0
+```
