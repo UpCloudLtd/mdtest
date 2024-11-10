669623743d8aada64344c1ae9acbd22871d33610
Toni Kangas <toni.kangas@upcloud.com>
2024-11-11 01:43:29 +0200

    feat: add `normalise` command

---
+++ cmd/normalise.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"github.com/UpCloudLtd/mdtest/utils"
+	"github.com/spf13/cobra"
+)
+
+var (
+	transforms []string
+	outputPath string
+
+	normaliseCmd = &cobra.Command{
+		Aliases: []string{"normalize"},
+		Use:     "normalise",
+		Short:   "Normalise the fenced code block info texts",
+		Long:    "Normalise the fenced code block info texts. By default, removes all info texts defined after the language identifier from the starting code-block fence.",
+		Args:    cobra.MinimumNArgs(1),
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(normaliseCmd)
+	normaliseCmd.Flags().StringArrayVarP(&transforms, "transform", "t", nil, "transform info text key in `old=new` format, e.g., `-t filename=title` would transform `filename` info text to `title` info text")
+	normaliseCmd.Flags().StringVarP(&outputPath, "output", "o", "", "`directory` where to save the normalised files")
+	_ = normaliseCmd.MarkFlagRequired("output")
+	normaliseCmd.RunE = func(cmd *cobra.Command, args []string) error {
+		cmd.SilenceUsage = true
+
+		params := utils.NormalizeParameters{
+			OutputPath: outputPath,
+			Transforms: transforms,
+		}
+
+		return utils.Normalize(args, params)
+	}
+}
--- testrun/testrun.go
+++ testrun/testrun.go
@@ -3,16 +3,13 @@ package testrun
 import (
 	"fmt"
 	"io"
-	"os"
-	"path"
-	"strings"
 	"time"
 
 	"github.com/UpCloudLtd/mdtest/id"
 	"github.com/UpCloudLtd/mdtest/output"
 	"github.com/UpCloudLtd/mdtest/testcase"
+	"github.com/UpCloudLtd/mdtest/utils"
 	"github.com/UpCloudLtd/progress"
-	"github.com/UpCloudLtd/progress/messages"
 )
 
 type RunParameters struct {
@@ -28,57 +25,6 @@ type RunResult struct {
 	SuccessCount int
 	FailureCount int
 	TestResults  []testcase.TestResult
-}
-
-type PathWarning struct {
-	path string
-	err  error
-}
-
-func (warn PathWarning) Message() messages.Update {
-	return messages.Update{
-		Message: fmt.Sprintf("Finding %s", warn.path),
-		Details: fmt.Sprintf("Error: %s", warn.err.Error()),
-		Status:  messages.MessageStatusWarning,
-	}
-}
-
-func parseFilePaths(rawPaths []string, depth int) ([]string, []PathWarning) {
-	paths := []string{}
-	warnings := []PathWarning{}
-	for _, rawPath := range rawPaths {
-		info, err := os.Stat(rawPath)
-		if err != nil {
-			warnings = append(warnings, PathWarning{rawPath, err})
-			if info == nil {
-				continue
-			}
-		}
-
-		if info.Mode().IsDir() && depth != 0 {
-			files, err := os.ReadDir(rawPath)
-			if err != nil {
-				warnings = append(warnings, PathWarning{rawPath, err})
-			}
-
-			dirRawPaths := []string{}
-			for _, file := range files {
-				dirRawPaths = append(dirRawPaths, path.Join(rawPath, file.Name()))
-			}
-
-			dirPaths, dirWarnings := parseFilePaths(dirRawPaths, depth-1)
-			if dirWarnings != nil {
-				warnings = append(warnings, dirWarnings...)
-			}
-
-			paths = append(paths, dirPaths...)
-		}
-
-		if strings.HasSuffix(rawPath, ".md") {
-			paths = append(paths, rawPath)
-		}
-	}
-	return paths, warnings
 }
 
 func PrintSummary(target io.Writer, run RunResult) {
@@ -102,7 +48,7 @@ func PrintSummary(target io.Writer, run RunResult) {
 
 func Execute(rawPaths []string, params RunParameters) RunResult {
 	started := time.Now()
-	paths, warnings := parseFilePaths(rawPaths, 1)
+	paths, warnings := utils.ParseFilePaths(rawPaths, 1)
 
 	testLog := progress.NewProgress(nil)
 	testLog.Start()
