8b8e5a3bfac4b456eb9e5cf489db32e9cceabac8
Toni Kangas <toni.kangas@upcloud.com>
2026-03-27 20:11:49 +0200

    chore: tune `--output-to-terminal` help and error texts

--- cmd/root.go
+++ cmd/root.go
@@ -33,13 +33,14 @@ func init() {
 	rootCmd.Flags().StringVar(&name, "name", "", "name for the testsuite to be printed into the console and to be used as the testsuite name in JUnit XML report")
 	rootCmd.Flags().StringVarP(&junitXML, "junit-xml", "x", "", "generate JUnit XML report to the specified `path`")
 	rootCmd.Flags().DurationVar(&timeout, "timeout", 0, "timeout for the test run as a `duration` string, e.g., 1s, 1m, 1h")
-	rootCmd.Flags().BoolVar(&outputToTerminal, "output-to-terminal", false, "print output from test steps directly to the terminal. Must be used with --jobs=1 to avoid collision of outputs from parallel tests")
+	rootCmd.Flags().BoolVar(&outputToTerminal, "output-to-terminal", false, "print output from `sh` blocks to the terminal in real-time. Only available when running tests non-concurrently: use either `--jobs=1` or target a single test file when using this flag.")
 
 	rootCmd.PreRunE = func(cmd *cobra.Command, args []string) error {
 		if outputToTerminal && numberOfJobs > 1 {
 			files, _ := utils.ParseFilePaths(args, 1)
 			if len(files) > 1 {
-				return fmt.Errorf("--output-to-terminal cannot be used when running tests in parallel on multiple test files. Please set --jobs=1 when using --output-to-terminal, or run tests with --output-to-terminal on a single file. Currently set --jobs to %d, files to be run: %d", numberOfJobs, len(files))
+				cmd.SilenceUsage = true
+				return fmt.Errorf("--output-to-terminal cannot be used when running tests in parallel. Please set --jobs=1 or target a single test file when using --output-to-terminal flag. Currently --jobs is set to %d and targeting %d files.", numberOfJobs, len(files))
 			}
 		}
 
